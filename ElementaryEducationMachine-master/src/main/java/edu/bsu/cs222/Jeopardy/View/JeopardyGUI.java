f11955327eaa107e4e11ca70260db427c1d88b62
Robert <ibsonnumber1@gmail.com>
2021-03-24 16:52:33 -0400

    Basic Jeopardy Working, examples for every category with every question worth 200

--- ElementaryEducationMachine-master/src/main/java/edu/bsu/cs222/Jeopardy/View/JeopardyGUI.java
+++ ElementaryEducationMachine-master/src/main/java/edu/bsu/cs222/Jeopardy/View/JeopardyGUI.java
@@ -3,6 +3,7 @@ package edu.bsu.cs222.Jeopardy.View;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -19,16 +20,16 @@ public class JeopardyGUI extends Application {
     @Override
     public void start(Stage primaryStage){
         VBox layout = new VBox(20);
-        buttonSetUp(layout);
+        boardSetUp(layout);
         primaryStage.setScene(new Scene(layout));
         primaryStage.show();
     }
 
     //sets up one column at a time, for the full 6
-    private void buttonSetUp(VBox layout) {
+    private void boardSetUp(VBox layout) {
+        GridPane gridPane = new GridPane();
         for(int i = 0; i<6; i++){
-            Text category = new Text("Example");
-            layout.getChildren().add(category);
+            gridPane.add(new Text("Example"),i,0);
             for(int j = 0; j<5; j++){
                 Button button = new Button();
                 button.setText("$200");
@@ -36,8 +37,9 @@ public class JeopardyGUI extends Application {
                     exampleQuestion.show();
                     button.setDisable(true);
                 });
-                layout.getChildren().add(button);
+                gridPane.add(button,i,j+1);
             }
         }
+        layout.getChildren().add(gridPane);
     }
 }
